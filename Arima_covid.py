040af25dffc7cf546b62f33a0a884f7874f43eca
Matteo Pastorino Ghezzi <matte.pasto@live.it>
2021-03-11 11:49:26 +0100

    Implementata Sarimax e messo a posto le exp smoothing

--- Arima_covid.py
+++ Arima_covid.py
@@ -1,10 +1,41 @@
-from pandas import read_csv
-from matplotlib import pyplot
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
+import statsmodels.tsa.statespace.sarimax as sarima
 from sklearn.metrics import mean_squared_error
 
-series = read_csv(r'C:\Users\matte\Documents\Covid_Machine_Learning\datasets\andamento-nazionale-completo.csv')
+series = pd.read_csv(r'C:\Users\matte\Documents\Covid_Machine_Learning\datasets\dpc-covid19-ita-andamento-nazionale.csv', index_col='data', parse_dates=True)
+X = series['nuovi_positivi'].values
+# X = X.asfreq('W')
 
+# training
+size = int(len(X)*0.8)
+X_train, X_test = X[0:size], X[size:len(X)]
+
+# ARIMA model
+darlt = sarima.SARIMAX(X_train, order=(1,1,3),trend='t').fit(disp=-1)
+darlf = darlt.forecast(steps=len(X_test))
+
+# plot
+fig, ax = plt.subplots()
+asse_x=np.arange(0,size,1)
+asse_x2=np.arange(size,len(X),1)
+ax.plot(asse_x,X_train, label='X_train')
+ax.plot(asse_x2,X_test, label='X_test')
+ax.plot(asse_x2,darlf, label='darlf')
+plt.legend(loc='upper left')
+plt.title('Prova')
+plt.ylabel('nuovi_positivi')
+plt.xlabel('Data')
+plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],
+ ["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+plt.show()
+
+
+
+'''
+---------------------------------------------------------------------
 # divido dataset in due: il primo è il training set che uso per il modello ARIMA; il secondo è il dataset di test che supponiamo non disponibile
 
 X = series['nuovi_positivi'].values
@@ -25,8 +56,9 @@ error = mean_squared_error(test, predictions)
 print('Test MSE: %.3f' % error)
 
 # plot
-pyplot.plot(test)
-pyplot.plot(predictions, color='red')
-pyplot.show()
+plt.plot(test)
+plt.plot(predictions, color='red')
+plt.show()
 
 # valori di arima non scelti da me; si può andare avanti/modificare impostando giusti valori
+'''
--- test.py
+++ test.py
@@ -64,6 +64,7 @@ ax.set(xlabel="Date",ylabel=variable,title=variable)
 date_form = DateFormatter("%d-%m")
 ax.xaxis.set_major_formatter(date_form)
 ax.xaxis.set_major_locator(mdates.DayLocator(interval = 3))
+plt.title('nuovi_positivi')
 #plt.show()
 
 fit1= SimpleExpSmoothing(nuovi_pos, initialization_method="heuristic").fit(smoothing_level=0.2,optimized=False)
@@ -85,6 +86,7 @@ line3, = plt.plot(fcast3, marker='o', color='green')
 plt.legend([line1, line2, line3], [fcast1.name, fcast2.name, fcast3.name])
 plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],
  ["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+plt.title('SingleExpSmoothing')
 #plt.show()      #04/09: ok fa robe: devo risolvere come mettere indici(probabilmente posso aggiungerlo in un metodo)
 
 # double exp smoothing
@@ -107,19 +109,55 @@ line_double3, = plt.plot(fcast_double3, marker='o', color='green')
 plt.legend([line_double1, line_double2, line_double3], [["alpha=%s, beta=%s", fit_double1.model.params['smoothing_level'], fit_double1.model.params['smoothing_trend']], ["alpha=%s, beta=%s", fit_double2.model.params['smoothing_level'], fit_double2.model.params['smoothing_trend']], ["alpha=%s, beta=%s", fit_double3.model.params['smoothing_level'], fit_double3.model.params['smoothing_trend']]])
 plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],
  ["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+plt.title('DoubleExpSmoothing')
 #plt.show()
 
 # triple exp smoothing: abbiamo notato seasonalità perché nei weekend nuovi_pos minori perché fatti meno tamponi
 
-fit_triple1= ExponentialSmoothing(nuovi_pos, trend= "add", seasonal= "add", seasonal_periods=7).fit()
-fcast_triple1 = fit_triple1.forecast(30)#.rename(r'$\alpha=%s, beta=%s$', fit_double1.model.params['smoothing_level'], fit_double1.model.params['smoothing_trend'])
-fit_triple2 = ExponentialSmoothing(nuovi_pos, trend= "add", seasonal= "mul", seasonal_periods=7).fit()
-fcast_triple2 = fit_triple2.forecast(30)#.rename(r'$\alpha=%s, beta=%s$', fit_double2.model.params['smoothing_level'], fit_double2.model.params['smoothing_trend'])
-fit_triple3 = ExponentialSmoothing(nuovi_pos, trend= "mul", seasonal= "add", seasonal_periods=7).fit()    # damped non va bene!
-fcast_triple3 = fit_triple3.forecast(30)#.rename(r'$\alpha=%s, beta=%s$', fit_double3.model.params['smoothing_level'], fit_double3.model.params['smoothing_trend'])
-fit_triple4 = ExponentialSmoothing(nuovi_pos, trend= "mul", seasonal= "mul", seasonal_periods=7).fit()    # damped non va bene!
-fcast_triple4 = fit_triple4.forecast(30)    # PROBABILMENTE LA MIGLIORE PAZZESCO
+size = int(len(nuovi_pos)*0.8)
+nuovi_pos_train=nuovi_pos[0:size]
+nuovi_pos_test=nuovi_pos[size:len(nuovi_pos)]
 
+fit_triple1= ExponentialSmoothing(nuovi_pos_train, trend= "add", seasonal= "add", seasonal_periods=7).fit()
+fcast_triple1 = fit_triple1.forecast(len(nuovi_pos)-size)#.rename(r'$\alpha=%s, beta=%s$', fit_double1.model.params['smoothing_level'], fit_double1.model.params['smoothing_trend'])
+fit_triple2 = ExponentialSmoothing(nuovi_pos_train, trend= "add", seasonal= "mul", seasonal_periods=7).fit()
+fcast_triple2 = fit_triple2.forecast(len(nuovi_pos)-size)#.rename(r'$\alpha=%s, beta=%s$', fit_double2.model.params['smoothing_level'], fit_double2.model.params['smoothing_trend'])
+fit_triple3 = ExponentialSmoothing(nuovi_pos_train, trend= "mul", seasonal= "add", seasonal_periods=7).fit()    # damped non va bene!
+fcast_triple3 = fit_triple3.forecast(len(nuovi_pos)-size)#.rename(r'$\alpha=%s, beta=%s$', fit_double3.model.params['smoothing_level'], fit_double3.model.params['smoothing_trend'])
+fit_triple4 = ExponentialSmoothing(nuovi_pos_train, trend= "mul", seasonal= "mul", seasonal_periods=7).fit()    # damped non va bene!
+fcast_triple4 = fit_triple4.forecast(len(nuovi_pos)-size)    # PROBABILMENTE LA MIGLIORE
+
+fig,axs=plt.subplots(2,2)
+axs[0,0].plot(nuovi_pos, marker='o', color='black')
+axs[0,0].plot(fit_triple1.fittedvalues, marker='o', color='blue')
+axs[0,0].plot(fcast_triple1, marker='o', color='cyan')
+plt.sca(axs[0, 0])
+plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+axs[0,0].set_title("add add")
+
+axs[0,1].plot(nuovi_pos, marker='o', color='black')
+axs[0,1].plot(fit_triple2.fittedvalues, marker='o', color='red')
+axs[0,1].plot(fcast_triple2, marker='o', color='cyan')
+plt.sca(axs[0, 1])
+plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+axs[0,1].set_title("add mul")
+
+axs[1,0].plot(nuovi_pos, marker='o', color='black')
+axs[1,0].plot(fit_triple3.fittedvalues, marker='o', color='green')
+axs[1,0].plot(fcast_triple3, marker='o', color='cyan')
+plt.sca(axs[1, 0])
+plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+axs[1,0].set_title("mul add")
+
+axs[1,1].plot(nuovi_pos, marker='o', color='black')
+axs[1,1].plot(fit_triple4.fittedvalues, marker='o', color='brown')
+axs[1,1].plot(fcast_triple4, marker='o', color='cyan')
+plt.sca(axs[1, 1])
+plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+axs[1,1].set_title("mul mul")
+fig.suptitle('TripleExpSmoothing')
+plt.show()
+'''
 plt.figure(figsize=(12, 8))
 plt.plot(nuovi_pos, marker='o', color='black')
 plt.plot(fit_triple1.fittedvalues, marker='o', color='blue')
@@ -133,7 +171,11 @@ line_triple4, = plt.plot(fcast_triple4, marker='o', color='brown')
 plt.legend([line_triple1, line_triple2, line_triple3, line_triple4], [["alpha=%s, beta=%s", fit_triple1.model.params['smoothing_level'], fit_triple1.model.params['smoothing_trend']], ["alpha=%s, beta=%s", fit_triple2.model.params['smoothing_level'], fit_triple2.model.params['smoothing_trend']], ["alpha=%s, beta=%s", fit_triple3.model.params['smoothing_level'], fit_triple3.model.params['smoothing_trend']],["alpha=%s, beta=%s", fit_triple4.model.params['smoothing_level'], fit_triple4.model.params['smoothing_trend']]])
 plt.xticks([0,5,10,15,20,25,30,35,40,45,50,55,60],
  ["1 Mar", "6 Mar", "11 Mar", "16 Mar", "21 Mar", "26 Mar", "31 Mar", "5 Apr", "10 Apr", "15 Apr", "20 Apr", "25 Apr", "30 Apr"])
+plt.title('TripleExpSmoothing')
 plt.show()
+'''
+
+
 """
 ti_index = pd.date_range(start='01/03/2020', end='30/04/2020')
 ti__data = pd.Series(ti, ti_index)
